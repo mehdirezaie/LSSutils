bee0daa6b903f5e3db33e1185f4178a08b4aebde
mehdirezaie <medirz90@icloud.com>
2020-04-09 21:00:10 -0400

    add monta env

--- LSSutils/dataviz.py
+++ LSSutils/dataviz.py
@@ -254,7 +254,7 @@ def mollview(m, vmin, vmax, unit, use_mask=False,
     '''    
     nside     = hp.npix2nside(len(m))
     rotatedeg = np.degrees(rotate)    
-    ysize     = xsize/2.                       # ratio is always 1/2
+    ysize     = int(xsize/2.)                     # ratio is always 1/2
 
     
     # galactic plane
