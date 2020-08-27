32c6a159a98d2e9289d0c42418751051c0f8023a
mehdirezaie <medirz90@icloud.com>
2020-08-27 12:28:14 -0400

    add hp func

---
+++ scripts/analysis/Nchains_eboss_ngc_main_known.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # The number of chains and angular C_ell
+
+# In[1]:
+
+
+import sys
+sys.path.insert(0, '/home/mehdi/github/LSSutils')
+
+
+# In[4]:
+
+
+import matplotlib.pyplot as plt
+
+import fitsio as ft
+import numpy as np
+import healpy as hp
+
+from lssutils.lab import get_cl, EbossCat, histogram_cell
+
+
+# In[5]:
+
+
+def normalize(nnmodel, nobsw, good):
+    """ normalize systematic weights such that N_{qso,tot} stays the same """
+    norm = (nobsw[good]/nnmodel[good]).sum()/nobsw[good].sum()
+    return norm*nnmodel
+
+def make_hp(hpix, values, nside=512):
+    """ make HEALPix """
+    mapi = np.zeros(12*nside*nside)
+    mapi[hpix] = values
+    return mapi
+
+# read data and randoms
+zmin = 0.8
+zmax = 2.2
+nside = 512
+
+cats_dir = '/home/mehdi/data/eboss/data/v7_2/'
+
+data = EbossCat(f'{cats_dir}eBOSS_QSO_full_NGC_v7_2.dat.fits', kind='data', zmin=0.8, zmax=2.2)
+randoms = EbossCat(f'{cats_dir}eBOSS_QSO_full_NGC_v7_2.ran.fits', kind='randoms', zmin=0.8, zmax=2.2)
+
+# to HEALPix
+nobs_w = data.to_hp(nside, zmin, zmax, raw=1)
+nran_w = randoms.to_hp(nside, zmin, zmax, raw=1)
+nran_mean = 5000.*hp.nside2pixarea(nside, degrees=True)
+
+
+nobs_def = data.to_hp(nside, zmin, zmax, raw=2)
+nran_def = randoms.to_hp(nside, zmin, zmax, raw=2)
+
+print(f'nran_mean: {nran_mean}')
+
+# new NN result
+wnn = ft.read(f'{cats_dir}1.0/NGC/512/main/nn_pnnl_known/nn-weights.fits')
+hpix = wnn['hpix']
+mask = np.zeros(12*nside*nside, '?')
+mask[hpix] = True
+
+# old NN result
+wnn_old = ft.read(f'{cats_dir}0.3/results/NGC_all_512/regression/nn_known/nn-weights512.fits')
+wnn_old_hp = make_hp(wnn_old['hpind'], wnn_old['weight'].mean(axis=1))
+
+
+# In[7]:
+
+
+assert np.array_equal(np.sort(wnn_old['hpind']), np.sort(wnn['hpix']))
+
+
+# In[8]:
+
+
+# compute C_ells
+
+kw = dict(njack=0, nran_bar=nran_mean)
+
+cl_old = get_cl(nobs_w, nran_w, mask, selection_fn=wnn_old_hp, **kw) # old NN
+cl_def = get_cl(nobs_def, nran_def, mask, **kw)                      # default systot
+
+cl_avg = {}
+cl_avg[-1] = get_cl(nobs_w, nran_w, mask, selection_fn=None, **kw)   # wo correction
+
+# average wNN over chains -> get C_ell
+for idx in range(1, wnn['weight'].shape[1]+1):
+
+    wsys = wnn['weight'][:, :idx].mean(axis=1)
+    wsys_hp = make_hp(hpix, wsys)
+
+    wsys_normed = normalize(wsys_hp, nobs_w, mask)
+
+    cl_avg[idx] = get_cl(nobs_w, nran_w, mask, selection_fn=wsys_normed, **kw)
+    print('.', end='')
+
+
+# get C_ell from chains -> average
+cl_ind = {}
+for idx in range(0, wnn['weight'].shape[1]):
+
+    wsys = wnn['weight'][:, idx]
+    wsys_hp = make_hp(hpix, wsys)
+
+    wsys_normed = normalize(wsys_hp, nobs_w, mask)
+
+    cl_ind[idx] = get_cl(nobs_w, nran_w, mask, selection_fn=wsys_normed, **kw)
+    print('.', end='')
+
+
+# In[13]:
+
+
+np.savez(f'{cats_dir}1.0/measurements/angular/cell_ngc_main_known_512_nchains.npz',
+        **{'cl_avg':cl_avg, 'cl_indv':cl_ind, 'cl_def':cl_def, 'cl_old':cl_old})
---
+++ scripts/analysis/run_cell_eboss.py
@@ -0,0 +1,107 @@
+
+import os
+
+from lssutils import setup_logging, CurrentMPIComm
+from lssutils.lab import get_cl, maps_eboss_v7p2, EbossCat
+from lssutils.utils import nside2pixarea, npix2nside
+
+import pandas as pd
+import numpy as np
+
+@CurrentMPIComm.enable
+def main(args, columns=maps_eboss_v7p2, comm=None):
+    
+    if comm.rank == 0:        
+        # --- only rank 0
+        zmin, zmax = args.zlim
+        nside = args.nside
+        use_systot = args.use_systot
+
+        
+        data_path = args.data_path
+        randoms_path = args.randoms_path
+        templates_path = args.templates_path
+        output_path = args.output_path
+
+        # read data, randoms, and templates
+        data = EbossCat(data_path, kind='data', zmin=zmin, zmax=zmax)
+        randoms = EbossCat(randoms_path, kind='randoms', zmin=zmin, zmax=zmax)
+
+        templates = pd.read_hdf(templates_path, key='templates')
+        sysm = templates[columns].values
+        if nside is not None:
+            assert sysm.shape[0] == 12*nside*nside, 'templates do not match with nside'
+        else:
+            nside = npix2nside(sysm.shape[0])
+        
+        # project to HEALPix
+        if use_systot:
+            ngal = data.to_hp(nside, zmin, zmax, raw=2)
+            nran = randoms.to_hp(nside, zmin, zmax, raw=2)
+        else:
+            ngal = data.to_hp(nside, zmin, zmax, raw=1)
+            nran = randoms.to_hp(nside, zmin, zmax, raw=1)
+
+        # construct the mask    
+        mask_nran = nran > 0
+        mask_ngal = np.isfinite(ngal)
+        mask_sysm = (~np.isfinite(sysm)).sum(axis=1) < 1
+
+        mask = mask_sysm & mask_nran & mask_ngal  
+
+        nran_bar = nside2pixarea(nside, degrees=True)*5000.
+
+    else:
+        ngal = None
+        nran = None
+        mask = None
+        sysm = None
+        nran_bar = None
+        
+        
+    ngal = comm.bcast(ngal, root=0)
+    nran = comm.bcast(nran, root=0)
+    mask = comm.bcast(mask, root=0)
+    sysm = comm.bcast(sysm, root=0)
+    nran_bar = comm.bcast(nran_bar, root=0)
+    
+    cls_list = get_cl(ngal, nran, mask, systematics=sysm, njack=20, nran_bar=nran_bar)
+    
+    if comm.rank == 0:
+        output_dir = os.path.dirname(output_path)
+        if not os.path.exists(output_dir):
+            print(f'creating {output_dir}')
+            os.makedirs(output_dir)
+       
+        np.save(output_path, cls_list)
+
+if __name__ == '__main__':
+    
+    setup_logging("info") # turn on logging to screen
+    comm = CurrentMPIComm.get()   
+    
+    if comm.rank == 0:
+        print(f'hi from {comm.rank}')
+        
+        from argparse import ArgumentParser
+        ap = ArgumentParser(description='Angular Power Spectrum Cell')
+        ap.add_argument('-d', '--data_path', required=True)
+        ap.add_argument('-r', '--randoms_path', required=True)
+        ap.add_argument('-t', '--templates_path', required=True)
+        ap.add_argument('-o', '--output_path', required=True)
+        ap.add_argument('-z', '--zlim', nargs='*', type=float, default=[0.8, 2.2]) 
+        ap.add_argument('-n', '--nside', type=int, default=None)
+        ap.add_argument('--use_systot', action='store_true')
+        ns = ap.parse_args()
+
+        for (key, value) in ns.__dict__.items():
+            print(f'{key:15s} : {value}')                
+    else:
+        ns = None
+        print(f'hey from {comm.rank}')
+        
+    main(ns)
+            
+            
+        
+    
