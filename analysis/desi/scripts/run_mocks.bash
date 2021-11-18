10f5b5b977cbc2e2792314ff6ab3dbef3dcd7ab7
Mehdi Rezaie <medirz90@icloud.com>
2021-11-18 13:04:07 -0500

    full sky mock mcmc works!

--- analysis/desi/scripts/run_mocks.bash
+++ analysis/desi/scripts/run_mocks.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=mockscl
 #SBATCH --account=PHS0336 
-#SBATCH --time=05:00:00
+#SBATCH --time=01:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
 #SBATCH --mail-type=FAIL
