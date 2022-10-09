e31a65d91960757051bcac7224598834d22974a8
Mehdi Rezaie <medirz90@icloud.com>
2022-10-09 17:42:59 -0400

    add robustness tests

--- analysis/desi/scripts/run_data.bash
+++ analysis/desi/scripts/run_data.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=nnfit
+#SBATCH --job-name=cell
 #SBATCH --account=PHS0336 
-#SBATCH --time=20:00:00
+#SBATCH --time=05:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=14
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=mr095415@ohio.edu
 
@@ -20,13 +20,13 @@ cd ${HOME}/github/LSSutils/analysis/desi/scripts/
 
 do_prep=false     # 20 min x 1 tpn
 do_lr=false       # 20 min x 1 tpn
-do_fit=true       # linmcmc:20m x 14, nn:20 h x 1 tpn
+do_fit=false       # linmcmc:20m x 14, nn:20 h x 1 tpn
 do_linsam=false   # 10 min x 1
 do_rfe=false       
 do_assign=false
 do_nbar=false     # 10 min x 4 tpn
 do_cl=false       # 20 min x 4 tpn
-do_mcmc=false     # 3 h x 14 tpn
+do_mcmc=true     # 3 h x 14 tpn
 do_mcmcf=false
 do_mcmc_joint=false # 3x14
 do_mcmc_joint3=false # 5x14
@@ -34,10 +34,10 @@ do_mcmc_joint3=false # 5x14
 bsize=5000    # 
 target='lrg'  # lrg
 region=$1     # bmzls, ndecals, sdecals, or desi
-maps="known1" # known, all, known1, known2
+maps="known1c" # known, all, known1, known2
 tag_d=0.57.0  # 0.57.0 (sv3) or 1.0.0 (main)
 nside=256     # lrg=256, elg=1024
-method="dnnp_known1"       # dnnp_known1, linp_known, or noweight
+method="dnnp_known1c"       # dnnp_known1, linp_known, or noweight
 fnltag="zero"
 model=dnnp    # dnnp, linp
 loss=pnll
@@ -178,6 +178,7 @@ then
     echo $output_path
     srun -n 4 python $nbar -d ${input_path} -o ${output_path} -s ${selection}            
 fi
+
 
 if [ "${do_cl}" = true ]
 then
